c4f5e9ad4f03b83b7708173bc498c0e02fb7a6a6
diguage <leejun119@gmail.com>
2018-12-09 13:06:39 +0800

    add Provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import Header from "./Header";
 import Content from "./Content";
 import "./index.css";
+import { Provider } from "./react-redux";
 
 function createStore(reducer) {
   let state = null;
@@ -35,14 +36,6 @@ const themeReducer = (state, action) => {
 const store = createStore(themeReducer);
 
 class Index extends Component {
-  static childContextTypes = {
-    store: PropTypes.object
-  };
-
-  getChildContext() {
-    return { store };
-  }
-
   render() {
     return (
       <div>
@@ -53,4 +46,9 @@ class Index extends Component {
   }
 }
 
-ReactDOM.render(<Index />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <Index />
+  </Provider>,
+  document.getElementById("root")
+);
